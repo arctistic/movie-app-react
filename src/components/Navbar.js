a3be095c1a4370229848ed8576c4f81a7218ab3d
Akhil Ratna Chettri <chettri.akhilratna@gmail.com>
2020-12-29 12:47:07 +0530

    Add UI, Actions, Reducers

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {data} from '../data';
+
+class Navbar extends React.Component {
+  
+    render(){
+        return (
+            <div className="nav">
+                <div className='search-container'>
+                    <input />
+                    <button id='search-btn'>Search</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
